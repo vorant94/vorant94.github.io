91e6dd944b017935ca1cfb74cb8bcff5af537adf
Mordechai Dror <vorant94@gmail.com>
2023-10-09 14:38:09 +0300

    posts page

--- packages/blog/src/shared/post.ts
+++ packages/blog/src/shared/post.ts
@@ -1,10 +1,17 @@
 import { type CollectionEntry, getEntry } from 'astro:content';
 import { compareDesc, format } from 'date-fns';
 import slugify from 'slugify';
+import { groupBy } from 'lodash';
 
 export class Post {
+  static dateFormatFull = 'MMM dd, yyyy';
+  static dateFormatShort = 'MMM dd';
+
   private static pathPrefix = 'posts';
-  private static dateFormat = 'MMM dd, yyyy';
+  private static slugifyOptions: Parameters<typeof slugify>[1] = {
+    lower: true,
+    remove: /['?]/g,
+  };
 
   private constructor(
     public readonly slug: string,
@@ -17,18 +24,18 @@ export class Post {
     return `${Post.pathPrefix}/${this.slug}`;
   }
 
-  static async fromEntry(entry: CollectionEntry<'posts'>): Promise<Post> {
+  static async fromEntry(
+    entry: CollectionEntry<'posts'>,
+    dateFormat = Post.dateFormatFull,
+  ): Promise<Post> {
     let slug: string = entry.slug;
     let { title, description } = entry.data;
-    const publishedAt = format(entry.data.publishedAt, Post.dateFormat);
+    const publishedAt = format(entry.data.publishedAt, dateFormat);
 
     if (entry.data.thread) {
       const thread = await getEntry(entry.data.thread);
 
-      slug = slugify(`${thread.data.title}-${slug}`, {
-        lower: true,
-        remove: /['?]/g,
-      });
+      slug = slugify(`${thread.data.title}-${slug}`, Post.slugifyOptions);
       title = `[${thread.data.title}]: ${title}`;
       description = thread.data.description;
     }
@@ -48,5 +55,11 @@ export class Post {
     return entries.sort((a, b) =>
       compareDesc(a.data.publishedAt, b.data.publishedAt),
     );
+  }
+
+  static groupEntriesByYear(
+    entries: CollectionEntry<'posts'>[],
+  ): Record<string, CollectionEntry<'posts'>[]> {
+    return groupBy(entries, ({ data }) => format(data.publishedAt, 'yyyy'));
   }
 }
