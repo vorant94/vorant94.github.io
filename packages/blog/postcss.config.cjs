85e23a2851df7a41bab68aacf7505335e7e15fea
Mordechai Dror <vorant94@gmail.com>
2023-10-17 17:01:31 +0300

    same thread posts

--- packages/blog/postcss.config.cjs
+++ packages/blog/postcss.config.cjs
@@ -1,4 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
-  plugins: [require('autoprefixer'), require('cssnano')],
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano'),
+    require('tailwindcss/nesting')(require('postcss-nested')),
+  ],
 };
