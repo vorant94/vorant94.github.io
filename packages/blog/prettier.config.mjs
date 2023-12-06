4f3410c67099193a82665e33b0ee32d86b27118c
Mordechai Dror <vorant94@gmail.com>
2023-12-06 08:41:49 +0200

    prettier plugin tailwindcss

--- packages/blog/prettier.config.mjs
+++ packages/blog/prettier.config.mjs
@@ -3,7 +3,12 @@ export default {
   singleQuote: true,
   bracketSameLine: true,
   singleAttributePerLine: true,
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-organize-imports'],
+  plugins: [
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-astro',
+    'prettier-plugin-organize-imports',
+  ],
+  tailwindConfig: './tailwind.config.ts',
   overrides: [
     {
       files: '*.astro',
