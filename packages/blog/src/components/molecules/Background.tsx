ee00c956220fe705308887e8d2bff91ebbf77c3e
Mordechai Dror <vorant94@gmail.com>
2023-12-14 13:57:17 +0200

    fixed #32

--- packages/blog/src/components/molecules/Background.tsx
+++ packages/blog/src/components/molecules/Background.tsx
@@ -3,7 +3,7 @@ import { type ReactElement } from 'react';
 
 export function Background(): ReactElement {
   const common =
-    'fixed z-0 h-[134px] w-[134px] lg:w-[300px] lg:h-[300px] rounded-full blur-[150px] md:blur-[350px] opacity-50 -z-10';
+    'fixed h-[134px] w-[134px] lg:w-[300px] lg:h-[300px] rounded-full blur-[150px] md:blur-[350px] opacity-50 -z-10';
 
   return (
     <>
