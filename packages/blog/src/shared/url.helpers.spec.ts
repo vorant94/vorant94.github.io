84f18d6807a8f22e783739608034eebcf724b3e6
Mordechai Dror <vorant94@gmail.com>
2024-01-24 13:52:47 +0200

    added example of tests

---
+++ packages/blog/src/shared/url.helpers.spec.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest';
+import { removeTrailingSlash } from './url.helpers.ts';
+
+describe(`url.helpers`, () => {
+  it(`should remove trailing slash from a string`, () => {
+    const input = 'https://www.vorant94.io/posts/';
+    const expected = 'https://www.vorant94.io/posts';
+
+    const actual = removeTrailingSlash(input);
+
+    expect(actual).toBe(expected);
+  });
+
+  it(`should keep input unmodified if it doesn't end up with slash`, () => {
+    const input = 'https://www.vorant94.io/posts';
+    const expected = 'https://www.vorant94.io/posts';
+
+    const actual = removeTrailingSlash(input);
+
+    expect(actual).toBe(expected);
+  });
+});
