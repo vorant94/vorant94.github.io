a2aae2da2fff25d81c47dae65289f7374eb07851
Mordechai Dror <vorant94@gmail.com>
2024-01-28 14:37:51 +0200

    custom utterances hook

--- packages/blog/src/components/Comments.tsx
+++ packages/blog/src/components/Comments.tsx
@@ -1,43 +1,61 @@
 import type { Post } from '@/shared';
-import { useEffect, useRef, type FunctionComponent } from 'react';
+import {
+  useEffect,
+  useRef,
+  type FunctionComponent,
+  type RefObject,
+} from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 export const Comments: FunctionComponent<CommentsProps> = function ({ post }) {
-  const ref = useRef<HTMLElement>(null);
+  const utterances = useThemedUtterances(post.slug);
+
+  return <section ref={utterances}></section>;
+};
+
+export interface CommentsProps {
+  post: Post;
+}
+
+function useThemedUtterances(issueTerm: string): RefObject<HTMLElement> {
+  const elRef = useRef<HTMLElement>(null);
 
   const isDark = useMediaQuery({ query: '(prefers-color-scheme: dark)' });
 
+  // since we are in the MPA-mode as a result of Astro, there is no need to clean up attached script
+  // on component disconnect, because disconnect happens only upon navigating out from current page
   useEffect(() => {
-    if (!ref.current) {
-      throw new Error(`Can't find ref to attach comments to`);
-    }
-
     const iframe =
       document.querySelector<HTMLIFrameElement>('.utterances-frame');
     // if iframe is found it means that effect is triggered by useMediaQuery and not by initial rendering,
     // hence we need to update theme on already attached iframe instead of attaching another one
     if (iframe) {
-      iframe.contentWindow?.postMessage(
-        { type: 'set-theme', theme: isDark ? 'github-dark' : 'github-light' },
-        'https://utteranc.es',
-      );
+      if (!iframe.contentWindow) {
+        throw Error(`Can't send theme change message`);
+      }
+
+      const message = {
+        type: 'set-theme',
+        theme: isDark ? 'github-dark' : 'github-light',
+      };
+      iframe.contentWindow.postMessage(message, 'https://utteranc.es');
     } else {
+      if (!elRef.current) {
+        throw new Error(`Can't find ref to attach comments to`);
+      }
+
       const scriptElem = document.createElement('script');
       scriptElem.src = 'https://utteranc.es/client.js';
       scriptElem.setAttribute('repo', 'vorant94/digital-garden');
-      scriptElem.setAttribute('issue-term', post.slug);
+      scriptElem.setAttribute('issue-term', issueTerm);
       scriptElem.setAttribute('label', 'comment');
       scriptElem.setAttribute('theme', isDark ? 'github-dark' : 'github-light');
       scriptElem.async = true;
       scriptElem.crossOrigin = 'anonymous';
 
-      ref.current.appendChild(scriptElem);
+      elRef.current.appendChild(scriptElem);
     }
   }, [isDark]);
 
-  return <section ref={ref}></section>;
-};
-
-export interface CommentsProps {
-  post: Post;
+  return elRef;
 }
