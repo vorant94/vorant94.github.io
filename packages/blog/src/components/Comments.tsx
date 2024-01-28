6cf6e40409f4b3de0b6624da4ad43bffb828cfdc
Mordechai Dror <vorant94@gmail.com>
2024-01-28 14:32:19 +0200

    made comments less messy

--- packages/blog/src/components/Comments.tsx
+++ packages/blog/src/components/Comments.tsx
@@ -5,39 +5,35 @@ import { useMediaQuery } from 'react-responsive';
 export const Comments: FunctionComponent<CommentsProps> = function ({ post }) {
   const ref = useRef<HTMLElement>(null);
 
-  const isDark = useMediaQuery(
-    { query: '(prefers-color-scheme: dark)' },
-    undefined,
-    (isDark) => {
-      const iframe =
-        document.querySelector<HTMLIFrameElement>('.utterances-frame');
-      if (!iframe) {
-        return;
-      }
+  const isDark = useMediaQuery({ query: '(prefers-color-scheme: dark)' });
 
+  useEffect(() => {
+    if (!ref.current) {
+      throw new Error(`Can't find ref to attach comments to`);
+    }
+
+    const iframe =
+      document.querySelector<HTMLIFrameElement>('.utterances-frame');
+    // if iframe is found it means that effect is triggered by useMediaQuery and not by initial rendering,
+    // hence we need to update theme on already attached iframe instead of attaching another one
+    if (iframe) {
       iframe.contentWindow?.postMessage(
         { type: 'set-theme', theme: isDark ? 'github-dark' : 'github-light' },
         'https://utteranc.es',
       );
-    },
-  );
+    } else {
+      const scriptElem = document.createElement('script');
+      scriptElem.src = 'https://utteranc.es/client.js';
+      scriptElem.setAttribute('repo', 'vorant94/digital-garden');
+      scriptElem.setAttribute('issue-term', post.slug);
+      scriptElem.setAttribute('label', 'comment');
+      scriptElem.setAttribute('theme', isDark ? 'github-dark' : 'github-light');
+      scriptElem.async = true;
+      scriptElem.crossOrigin = 'anonymous';
 
-  useEffect(() => {
-    if (!ref.current) {
-      return;
+      ref.current.appendChild(scriptElem);
     }
-
-    const scriptElem = document.createElement('script');
-    scriptElem.src = 'https://utteranc.es/client.js';
-    scriptElem.setAttribute('repo', 'vorant94/digital-garden');
-    scriptElem.setAttribute('issue-term', post.slug);
-    scriptElem.setAttribute('label', 'comment');
-    scriptElem.setAttribute('theme', isDark ? 'github-dark' : 'github-light');
-    scriptElem.async = true;
-    scriptElem.crossOrigin = 'anonymous';
-
-    ref.current.appendChild(scriptElem);
-  }, [ref, isDark]);
+  }, [isDark]);
 
   return <section ref={ref}></section>;
 };
