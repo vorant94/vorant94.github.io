559ec53399869ddf8af2ad48f679d3067e3f01e5
Mordechai Dror <vorant94@gmail.com>
2024-01-31 09:45:16 +0200

    added slide in, slide out animations

--- packages/blog/src/layouts/layout/Modal.tsx
+++ packages/blog/src/layouts/layout/Modal.tsx
@@ -10,15 +10,31 @@ import {
   type SetStateAction,
 } from 'react';
 import { Header } from './Header';
+import styles from './Modal.module.css';
 
 export const Modal: FunctionComponent<PropsWithChildren<ModalProps>> =
   function ({ children, isOpen, onCloseClick }) {
+    // TODO make a separate SlideInSlideOut component, challenges:
+    //  - it seems that css modules add a wrapping div, so position is messed up when animation is on its own
+    //  - need to support triggering the slideOut animation from outside like here with close button
+    const [isSlidingOut, setIsSlidingOut] = useState(false);
+
     return (
       isOpen && (
-        <div className="fixed top-0 left-0 w-dvw h-dvh backdrop-filter backdrop-blur z-10">
+        <div
+          className={classNames(
+            'fixed top-0 left-0 w-dvw h-dvh backdrop-filter backdrop-blur z-10',
+            isSlidingOut ? styles.slidingOut : styles.slidingIn,
+          )}
+          onAnimationEnd={() => {
+            if (isSlidingOut) {
+              onCloseClick();
+              setIsSlidingOut(false);
+            }
+          }}>
           <div className={classNames(THEME.fullWidth, 'flex flex-col h-full')}>
             <Header>
-              <Button onClick={onCloseClick}>
+              <Button onClick={() => setIsSlidingOut(true)}>
                 <Icon glyph="close" />
               </Button>
             </Header>
--- packages/blog/src/layouts/layout/Nav/Nav.tsx
+++ packages/blog/src/layouts/layout/Nav/Nav.tsx
@@ -11,7 +11,7 @@ import { Modal, useIsModalOpen } from '../Modal.tsx';
 import { NavLink, type NavLinkComponent } from './NavLink.tsx';
 
 const Nav: NavComponent = function ({ children }) {
-  const [isModalOpen, setIsModalOpen] = useMobileNavModal();
+  const [isMobileNavOpen, setIsMobileNavOpen] = useIsMobileNavOpen();
 
   return (
     <>
@@ -21,13 +21,13 @@ const Nav: NavComponent = function ({ children }) {
 
       <Button
         className="lg:hidden"
-        onClick={() => setIsModalOpen(!isModalOpen)}>
+        onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}>
         <Icon glyph="menu" />
       </Button>
 
       <Modal
-        isOpen={isModalOpen}
-        onCloseClick={() => setIsModalOpen(false)}>
+        isOpen={isMobileNavOpen}
+        onCloseClick={() => setIsMobileNavOpen(false)}>
         <nav className="flex-1 flex flex-col justify-center">
           <ul className="flex flex-col">{children}</ul>
         </nav>
@@ -36,7 +36,7 @@ const Nav: NavComponent = function ({ children }) {
   );
 };
 
-function useMobileNavModal(): [boolean, Dispatch<SetStateAction<boolean>>] {
+function useIsMobileNavOpen(): [boolean, Dispatch<SetStateAction<boolean>>] {
   const [isModalOpen, setIsModalOpen] = useIsModalOpen();
 
   const isLgScreen = useMediaQuery({ query: '(min-width: 1024px)' });
