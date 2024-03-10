4f37e784c2e103d43bd0d209676a88edca175118
Mordechai Dror <vorant94@pm.me>
2024-03-10 21:00:52 +0200

    fix small ui bugs

--- packages/blog/src/components/PostList/PostListItem.tsx
+++ packages/blog/src/components/PostList/PostListItem.tsx
@@ -17,10 +17,13 @@ export const PostListItem: FunctionComponent<PostListItemProps> = function ({
     <li
       className={cn('flex flex-col py-3 text-medium', className)}
       {...rest}>
+      {/*can't use dg-primary-text here because tw-merge doesn't catch the overwrite*/}
       <Link
         href={getPostFullPath(post)}
         prefetch="hover"
-        className={cn('dg-primary-text', 'flex gap-3 items-center')}
+        className={cn(
+          'text-slate-800 dark:text-slate-100 flex gap-3 items-center',
+        )}
         aria-label={post.data.title}>
         <span className="flex-1 truncate">{post.data.title}</span>
         <span className="whitespace-nowrap text-xs">
