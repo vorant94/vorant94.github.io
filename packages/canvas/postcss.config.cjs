b1b39eab206a0a5199f0fec7648edcd1c503fdde
Mordechai Dror <vorant94@pm.me>
2024-03-13 13:57:11 +0200

    polish

--- packages/canvas/postcss.config.cjs
+++ packages/canvas/postcss.config.cjs
@@ -1,4 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
-  plugins: [require('tailwindcss')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano'),
+    require('tailwindcss/nesting')(require('postcss-nested')),
+  ],
 };
