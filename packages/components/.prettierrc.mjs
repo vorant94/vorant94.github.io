f7366a68a474505e9a9e97a561def77ff42d8ecc
Mordechai Dror <vorant94@pm.me>
2024-03-15 09:00:33 +0200

    remove prettier

--- packages/components/.prettierrc.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-  singleQuote: true,
-  bracketSameLine: true,
-  singleAttributePerLine: true,
-  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-organize-imports'],
-  tailwindConfig: './tailwind.config.ts',
-};
--- packages/utils/.prettierrc.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-  singleQuote: true,
-  bracketSameLine: true,
-  singleAttributePerLine: true,
-  plugins: ['prettier-plugin-organize-imports'],
-};
